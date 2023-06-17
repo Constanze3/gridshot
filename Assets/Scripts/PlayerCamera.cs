20de0d679f66f2594b465af3df04eaeeeb9fa761
Constanze3 <81444666+Constanze3@users.noreply.github.com>
2023-06-17 03:40:29 +0200

    camera controller

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensitivity = 2f;
+
+    private Vector2 rotation = Vector2.zero;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        rotation.x += Input.GetAxis("Mouse X") * sensitivity;
+        if (rotation.x < 0)
+        {
+            rotation.x += 360;
+        }
+        else if (360 < rotation.x)
+        {
+            rotation.x -= 360;
+        }
+
+        rotation.y += Input.GetAxis("Mouse Y") * sensitivity;
+        rotation.y = Mathf.Clamp(rotation.y, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(-rotation.y, rotation.x, transform.localRotation.z);
+
+        Debug.Log(rotation);
+    }
+}
